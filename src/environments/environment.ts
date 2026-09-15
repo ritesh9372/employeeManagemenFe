8696a687e37524b2a375489ab0223d4592a3725f
Dheeraj <dheerajvishwakarma07@gmail.com>
2026-09-15 15:13:24 +0530

    RY-correction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://employeemanagemenbe.onrender.com/'
+  apiUrl: 'https://employeemanagemenbe.onrender.com'
   // apiUrl: 'http://localhost:3000/api'
 };
